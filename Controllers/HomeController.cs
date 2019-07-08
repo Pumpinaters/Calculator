4bc6e61907257ad3748190eac8faeae8635d9415
Pumpinaters <39046117+Pumpinaters@users.noreply.github.com>
2019-07-08 19:22:19 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using MattTest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MattTest2.Controllers
+{
+    public class HomeController : Controller { 
+        //    
+        // GET: /Home/    
+        [HttpGet]
+        public ActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult Index(CalculatorViewModel model, string command)
+    {
+        if (command == "add")
+        {
+            model.Result = model.A + model.B;
+        }
+        if (command == "sub")
+        {
+            model.Result = model.A - model.B;
+        }
+        if (command == "mul")
+        {
+            model.Result = model.A * model.B;
+        }
+        if (command == "div")
+        {
+            model.Result = model.A / model.B;
+        }
+        return View(model);
+    }
+}    
+}
---
+++ Models/CalculatorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MattTest2.Models
+{
+    public class CalculatorViewModel
+    {
+        //public int A { get; set; }    
+        //public int B { get; set; }    
+        //public int Result { get; set; }    
+        public double A
+        {
+            get;
+            set;
+        }
+        public double B
+        {
+            get;
+            set;
+        }
+        public double Result
+        {
+            get;
+            set;
+        }
+    }
+}
